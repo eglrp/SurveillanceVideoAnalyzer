ef605dbac8816b2aca3729ac840e0c24eab3fe93
zhengxuping <xupingzheng@gmail.com>
2014-10-15 13:53:41 +0800

    修改了Timer.h

--- Test/MainTaicangProcVideo.cpp
+++ Test/MainTaicangProcVideo.cpp
@@ -39,14 +39,17 @@ void callBack(float progressPercentage, const vector<zpv::TaicangObjectInfo>& in
 
 int main(int argc, char** argv)
 {
-    string videoPath = /*"D:\\SHARED\\TrimpsVideo\\20140320\\Video09_2014_03_20_13_05_15_type1.avi"*/
+    string videoPath = 
+    /*"D:\\SHARED\\XinjiangVideo\\2014-08-31\\21.102.10.103_06_20140831193904656.mp4"*/
+    "D:\\SHARED\\XinjiangVideo\\2014-08-30\\21.102.10.103_03_20140830182449984.mp4"
+    /*"D:\\SHARED\\TrimpsVideo\\20140320\\Video09_2014_03_20_13_05_15_type1.avi"*/
     /*"D:\\SHARED\\TrimpsVideo\\20140320\\Video06_2014_03_20_13_00_15_type1.avi"*/
     /*"D:\\SHARED\\TrimpsVideo\\20140316\\01\\Video01_2014_03_16_14_57_28_type1.avi"*/
     /*"D:/SHARED/GuilinVideo/DVR1_灵川八里街川东一路八里一路路口20130909070538.avi"*/
     /*"D:\\SHARED\\MiscellaneousVideo\\DCS-7010L_20140416160622.avi"*/
     /*"D:\\SHARED\\MiscellaneousVideo\\案发前20秒(2).avi"*/
 	/*"D:/SHARED/TaicangVideo/1/70.flv"*/
-    "D:/SHARED/TaicangVideo/227省道、北门街_0_2014-08-20_10-09-08.asf"
+    /*"D:/SHARED/TaicangVideo/227省道、北门街_0_2014-08-20_10-09-08.asf"*/
     /*"D:/SHARED/TaicangVideo/209_32058517001310010090_40_2014-08-07 08_42_35~2014-08-07 08_47_35_bak.avi"*/
     /*"D:\\SHARED\\MiscellaneousVideo\\video\\4M2D12-21-2C.avi"*/
     /*"D:\\SHARED\\MiscellaneousVideo\\video\\test1030.avi"*/
--- Tool/Timer.h
+++ Tool/Timer.h
@@ -35,22 +35,21 @@ class RepeatTimer
 public:
     RepeatTimer(void) 
     {
-        freq = cv::getTickFrequency(); 
         clear();
     };
     void clear(void) 
     {
-        startTime = 0; 
         accTime = 0; 
         count = 0;
     }
     void start(void) 
     {
-        startTime = cv::getTickCount();
+        timer.start();
     };
     void end(void) 
     {
-        accTime += (double(cv::getTickCount() - startTime) / freq); 
+        timer.end();
+        accTime += timer.elapse(); 
         count++;
     };
     double getAccTime(void) 
@@ -66,8 +65,79 @@ public:
         return count == 0 ? 0 : (accTime / count);
     }
 private:    
-    double freq;
-    long long int startTime;
+    Timer timer;
+    double accTime;
+    int count;
+};
+
+class InterruptTimer
+{
+public:
+    InterruptTimer(void)
+    {
+        accTime = 0;
+    }
+    void start(void)
+    {
+        timer.start();
+    }
+    void end(void)
+    {
+        timer.end();
+        accTime += timer.elapse();
+    }
+    double elapse(void)
+    {
+        return accTime;
+    }
+    void clear(void)
+    {
+        accTime = 0;
+    }
+private:
+    Timer timer;
+    double accTime;
+};
+
+class RepeatInterruptTimer
+{
+public:
+    RepeatInterruptTimer(void)
+    {
+        clear();
+    }
+    void clear(void)
+    {
+        accTime = 0;
+        count = 0;
+    }
+    void start(void)
+    {
+        timer.start();
+    }
+    void end(void)
+    {
+        timer.end();
+        accTime += timer.elapse();
+    }
+    void increaseCount(void)
+    {
+        count++;
+    }
+    double getAccTime(void) 
+    {
+        return accTime;
+    };
+    int getCount(void) 
+    {
+        return count;
+    };
+    double getAvgTime(void) 
+    {
+        return count == 0 ? 0 : (accTime / count);
+    }
+private:
+    InterruptTimer timer;
     double accTime;
     int count;
 };
